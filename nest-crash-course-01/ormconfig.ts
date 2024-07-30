195ce14d133cf0c22a034d0c689a1be8f95c7200
yogeshkanwade21 <yogeshkanwade2015@gmail.com>
2024-07-30 11:32:18 +0530

    setup postgres in docker and add user table using TypeORM

---
+++ nest-crash-course-01/ormconfig.ts
@@ -0,0 +1,18 @@
+import { User } from "src/entities/user.entity";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+
+
+const ormConfig: PostgresConnectionOptions = {
+    type: 'postgres',
+    database: 'testDB',
+    host: 'localhost',
+    port: 5432,
+    username: 'yogesh',
+    password: '12345',
+    entities: [User],
+    synchronize: true, // not recommended for production
+    //Automatically creates or updates database tables based on your entity definitions.
+    // This means whenever your application starts, TypeORM will compare your entity structure with the database schema and make necessary changes.
+}
+
+export default ormConfig;
--- nest-crash-course-01/src/app.module.ts
+++ nest-crash-course-01/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CommentModule } from './comment/comment.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import ormConfig from 'ormconfig';
 
 @Module({
-  imports: [UserModule, CommentModule],
+  imports: [UserModule, CommentModule, TypeOrmModule.forRoot(ormConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
